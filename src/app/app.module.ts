992f560f5f10520f3561ba0a298a3b956afd152a
kyungjoongo <kyungjoon.go@gmail.com>
2017-11-23 09:17:43 +0900

    sdfgsdfgsdfgsdfgsdfg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,68 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {GooglePlus} from '@ionic-native/google-plus';
+import {HttpModule} from '@angular/http';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireModule} from 'angularfire2';
+import {FirebaseProvider} from '../providers/firebase/firebase';
+
+import {HttpClientModule} from "@angular/common/http";
+import {HttpProvider} from '../providers/http/http';
+import { LocalStorageModule } from 'angular-2-local-storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8pHuo5qMXU0Wk9tRJxsrjsrQ1FGMrbW4",
+    authDomain: "fir-001-9e43a.firebaseapp.com",
+    databaseURL: "https://fir-001-9e43a.firebaseio.com",
+    projectId: "fir-001-9e43a",
+    storageBucket: "fir-001-9e43a.appspot.com",
+    messagingSenderId: "96753856506"
+};
+
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        ListPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        HttpModule, HttpClientModule,
+        AngularFireDatabaseModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        LocalStorageModule.withConfig({
+            prefix: 'kyungjoon-app001',
+            storageType: 'localStorage'
+        }),
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        ListPage
+    ],
+    providers: [
+
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}, GooglePlus,
+        FirebaseProvider,
+        HttpProvider
+
+
+    ]
+
+
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,153 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {GooglePlus} from "@ionic-native/google-plus";
+import {FirebaseListObservable} from 'angularfire2/database';
+import {FirebaseProvider} from "../../providers/firebase/firebase";
+import {HttpProvider} from "../../providers/http/http";
+import {LocalStorageService} from 'angular-2-local-storage';
+import {ListPage} from "../list/list";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    savedUserId: string;
+    shoppingItems: FirebaseListObservable<any[]>;
+    newItem = {};
+    displayName: any;
+    email: any;
+    familyName: any;
+    givenName: any;
+    userId: any;
+    imageUrl: any;
+    isLoggedIn: boolean = false;
 
-  }
+
+    constructor(public navCtrl: NavController, private googlePlus: GooglePlus
+        , public firebaseProvider: FirebaseProvider
+        , private httpProvider: HttpProvider
+        , private localStorageService: LocalStorageService) {
+
+
+    }
+
+    ionViewDidEnter() {
+
+        /* if (this.localStorageService.get("userId") === null) {
+             console.log('널이네');
+         } else {
+             let savedUserId = this.localStorageService.get("userId").toString();
+             console.log(savedUserId);
+
+             this.login();
+         }
+ */
+
+    }
+
+
+    login() {
+
+        /**
+         *
+         * 일치 한는 정보가 있는지를 db와 oatuh에서 확인하여
+         *
+         * 일치한느 정보가 있으면 list페이지로
+         * 그렇지 않으면
+         * 가입 페이지를 보여준다.
+         *
+         */
+
+
+        this.googlePlus.login({}).then(loginResponse => {
+
+
+            this.httpProvider.getItem(loginResponse.userId).then(res => {
+
+                if (res != null) {
+                    console.log('이미 가입된 아이디가 있습니다')
+
+                    this.navCtrl.push(ListPage)
+                }else{
+                    this.displayName = loginResponse.displayName;
+                    this.email = loginResponse.email;
+                    this.familyName = loginResponse.familyName;
+                    this.givenName = loginResponse.givenName;
+                    this.userId = loginResponse.userId;
+                    this.imageUrl = loginResponse.imageUrl;
+                    this.isLoggedIn = true;
+                    console.log(this.displayName);
+
+                }
+            })
+
+
+
+
+
+            /**
+             * 해당id가 있어서 페치가 되어진 경우는 아이디가 존재하는 것이므로 리다이렉트.
+             */
+
+
+            // this.localStorageService.set('userId', this.userId);
+
+        })
+            .catch(err => {
+
+                alert('애러네');
+                console.error(err)
+            });
+
+        //alert('sdflksdlkflsdkf')
+
+    }
+
+    logout() {
+        this.googlePlus.logout()
+            .then(res => {
+                console.log(res);
+                this.displayName = "";
+                this.email = "";
+                this.familyName = "";
+                this.givenName = "";
+                this.userId = "";
+                this.imageUrl = "";
+
+                this.isLoggedIn = false;
+
+                alert('로그아웃되었어요!');
+            })
+            .catch(err => console.error(err));
+    }
+
+
+    register() {
+
+        /*this.httpProvider.addItem()*/
+
+        let userInfo = {
+            displayName: this.displayName,
+            email: this.email,
+            familyName: this.familyName,
+            givenName: this.givenName,
+            userId: this.userId,
+            imageUrl: this.imageUrl
+        }
+
+
+        this.httpProvider.addItem(userInfo).then(res => {
+
+            console.log("res->" + JSON.stringify(res));
+
+            alert('가입되었어요!');
+            this.navCtrl.push(ListPage)
+
+        });
+
+    }
+
 
 }
